fa7d4ceee1b82a769ee5275cc722823a097dbdab
janaee0143 <courtneyjanae.billingsleygreer@calbaptist.edu>
2020-10-21 23:00:28 -0700

    added variables for classes

--- app/src/main/java/com/nosymptoms/questionnaireapp/QuestionnaireActivity.java
+++ app/src/main/java/com/nosymptoms/questionnaireapp/QuestionnaireActivity.java
@@ -3,15 +3,22 @@ package com.nosymptoms.questionnaireapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QuestionnaireActivity extends AppCompatActivity {
 
+    Button photo;
+    Button submit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire);
+
+        photo = (Button)findViewById(R.id.photo_button);
+        submit = (Button)findViewById(R.id.submit_questionnaire);
     }
 
     public void onSubmitButtonClick(View view) {

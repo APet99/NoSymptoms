a9f5035dd1d46a6168db62b2c9d32bbf3d4b61dd
Cameron Warren <cameronjoseph.warren@calbaptist.edu>
2020-11-04 17:04:20 -0800

    Resource page implementation with two sources

--- app/src/main/java/com/nosymptoms/questionnaireapp/HomeActivity.java
+++ app/src/main/java/com/nosymptoms/questionnaireapp/HomeActivity.java
@@ -1,22 +1,27 @@
 package com.nosymptoms.questionnaireapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Layout;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 import com.nosymptoms.questionnaireapp.ui.main.SectionsPagerAdapter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HomeActivity extends AppCompatActivity {
+
+    Map<Integer,String> linkMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,21 @@ public class HomeActivity extends AppCompatActivity {
 
     public void onCheckInButtonClick(View view) {
         Intent intent = new Intent(this, QuestionnaireActivity.class);
+        startActivity(intent);
+    }
+
+    public void onResourceButtonClick(View view) {
+        if (linkMap == null) {
+            linkMap = new HashMap<>();
+            ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.resource_fragment);
+            String[] links = getResources().getStringArray(R.array.links);
+            for (int i = 0; i < layout.getChildCount(); i++) {
+                linkMap.put(layout.getChildAt(i).getId(), links[i]);
+            }
+        }
+
+        String link = linkMap.get(view.getId());
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
         startActivity(intent);
     }
 }

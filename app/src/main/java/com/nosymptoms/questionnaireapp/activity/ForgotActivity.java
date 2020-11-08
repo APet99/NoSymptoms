b8f0e03a024f251ffa304ab203a8af385e28d275
janaee0143 <courtneyjanae.billingsleygreer@calbaptist.edu>
2020-11-07 21:32:40 -0800

    updated Register and login to work with DB. Added procedure for forgot.

--- app/src/main/java/com/nosymptoms/questionnaireapp/activity/ForgotActivity.java
+++ app/src/main/java/com/nosymptoms/questionnaireapp/activity/ForgotActivity.java
@@ -8,30 +8,70 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nosymptoms.questionnaireapp.R;
+import com.nosymptoms.questionnaireapp.dao.UserDao;
+import com.nosymptoms.questionnaireapp.model.User;
+
+import javax.inject.Inject;
 
 public class ForgotActivity extends AppCompatActivity {
 
-    EditText email_forgot;
-    EditText user_forgot;
+    EditText user_id_forgot;
     Button send_email;
     TextView login_forgot;
+
+    @Inject
+    UserDao userDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot);
 
-        email_forgot = (EditText)findViewById(R.id.email_forgot);
-        user_forgot = (EditText)findViewById(R.id.username_forgot);
+        user_id_forgot = (EditText)findViewById(R.id.id_forgot);
         send_email = (Button)findViewById(R.id.send_email);
         login_forgot = (TextView)findViewById(R.id.login_forgot);
     }
 
     public void onSendButtonClick(View view) {
         //TODO: Add recovery procedure
-        Intent intent = new Intent(this, LoginActivity.class);
+
+        boolean isUserValid = isUserInDB(user_id_forgot.toString());
+
+        if(isUserValid){
+            //get security question
+//            String securityQuestion = userDAO.getUserById(Integer.parseInt(user_id_forgot.toString())).getSecurityQuestion();
+            //display security question in TextView and text for response
+
+
+            //if the text matches the user's response in DB
+            //then let user access the Change Password Activity
+
+            Intent intent = new Intent(this, LoginActivity.class); //send to Change Pass Activity
+            startActivity(intent);
+
+            //change user's password in the database in Change Pass Activity
+            //send to login activity
+        }
+
+    }
+
+    private boolean isUserInDB (String userID){
+        User userToCheck = userDAO.getUserById(Integer.parseInt(userID));
+
+        //check if the username and password match a user in the database
+        if((userToCheck != null)){
+            Toast.makeText(getApplicationContext(), "User does not exist. Create an account.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void onRegisterButtonClick(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
 
